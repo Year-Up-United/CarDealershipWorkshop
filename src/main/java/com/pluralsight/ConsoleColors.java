7b3d8428e949cd02b3c84a2fdd2e3783d682e628
thejaylajones <janeissaj@gmail.com>
2025-06-13 13:08:19 -0400

    making changes

--- src/main/java/com/pluralsight/ConsoleColors.java
+++ src/main/java/com/pluralsight/ConsoleColors.java
@@ -5,5 +5,10 @@ public class ConsoleColors {
     // colors for the console
     public static final String BARBIE_PINK = "\u001B[95m";
     public static final String RESET = "\u001B[0m";
-
+    public static final String HOT_PINK = "\u001B[91m";         // BRIGHT RED (HOT PINK)
+    public static final String LIGHT_PINK = "\u001B[35m";       // MAGENTA (LIGHT PINK)
+    public static final String BOLD = "\u001B[1m";              // BOLD TEXT
+    public static final String UNDERLINE = "\u001B[4m";         // UNDERLINE TEXT
+    public static final String BRIGHT_WHITE = "\u001B[97m";     // BRIGHT WHITE
+    public static final String PINK_BG = "\u001B[105m";         // PINK BACKGROUND
 }
